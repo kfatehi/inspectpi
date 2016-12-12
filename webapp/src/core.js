93c7a6ccd2a408260d1092cfc1e2f03013e65583
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-12-12 00:15:01 -0800

    say hi

--- webapp/src/core.js
+++ webapp/src/core.js
@@ -3,6 +3,7 @@ class Core {
     this.io = io;
     io.on('connection', socket => {
       socket.on('action', this.handleAction)
+      socket.emit('action', { type: "HELLO" });
     });
   }
   handleAction (action) {
