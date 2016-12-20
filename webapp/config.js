4381dc6471e2f50a4486f3e756f6e44ccfde3d47
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-12-19 21:28:47 -0800

    add hterm

--- webapp/config.js
+++ webapp/config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  pluginPath: __dirname+'/plugins',
+  activePlugins: [],
   sdCardDevicePath: '/dev/sda',
   imagesPath: '/home/pi/images',
-  stateFile: '/home/pi/inspectpi.state'
+  stateFile: '/home/pi/inspectpi.state',
 }
